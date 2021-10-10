42b5996d10bab8e9db87312382c24863ba458759
nico-limo <nicolas.limonoff@gmail.com>
2021-10-10 16:55:35 -0300

    front done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import { getWallet, getWeb3 } from "./utils";
+import Header from "./components/Header";
+import NewTransfer from "./components/NewTransfer";
+import TransferList from "./components/TransferList";
+// Need run truffle develop to use our contract in local way
 
-function App() {
+const App = () => {
+  const [web3, setWeb3] = useState(undefined);
+  const [accounts, setAccounts] = useState(undefined);
+  const [wallet, setWallet] = useState(undefined);
+  const [approvers, setApprovers] = useState(undefined);
+  const [quorum, setQuorum] = useState(undefined);
+  const [transfers, setTransfers] = useState([])
+
+  useEffect(() => {
+    const init = async () => {
+      const web3 = await getWeb3();
+      const accounts = await web3.eth.getAccounts();
+      const wallet = await getWallet(web3);
+      const approvers = await wallet.methods.getApprovers().call();
+      const quorum = await wallet.methods.quorum().call();
+      const transfers = await wallet.methods.getTransfers().call();
+      setWeb3(web3);
+      setAccounts(accounts);
+      setWallet(wallet);
+      setApprovers(approvers);
+      setQuorum(quorum);
+      setTransfers(transfers)
+    };
+    init();
+  }, [])
+
+  const createTransfer = async transfer => {
+   await wallet.methods
+      .createTransfer(transfer.amount, transfer.to)
+      .send({ from: accounts[0] });
+
+      const transfers = await wallet.methods.getTransfers().call();
+      setTransfers(transfers);
+    
+  }
+
+  const approveTransfer = async (transferId) => {
+   await wallet.methods
+      .approveTransfer(transferId)
+      .send({ from: accounts[0] });
+        const transfers = await wallet.methods.getTransfers().call();
+        setTransfers(transfers);
+  }
+
+  if (
+    typeof web3 === 'undefined'
+    || typeof accounts === 'undefined'
+    || typeof wallet === 'undefined'
+    || typeof approvers === 'undefined'
+    || typeof quorum === 'undefined'
+  ) {
+    return <p>Loading...</p>
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      Multisig Dap
+      <Header approvers={approvers} quorum={quorum} />
+      <NewTransfer createTransfer={createTransfer} />
+      <TransferList transfers={transfers} approveTransfer={approveTransfer} />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
